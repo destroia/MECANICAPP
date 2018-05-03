8b12f74953f0c46d43358bca745a22b9482db319
destroia <davidstiven123@gmail.com>
2018-05-03 11:02:50 -0500

    03/05/2018

---
+++ MECANICAPP/MECANICAPP/Models/CategoryMap.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MECANICAPP.Models
+{
+    public class CategoryMap
+    {
+        public int CategoryId { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
--- MECANICAPP/MECANICAPP/ViewModels/EmpresaViewModel/EmpresaInicioViewModel.cs
+++ MECANICAPP/MECANICAPP/ViewModels/EmpresaViewModel/EmpresaInicioViewModel.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using MECANICAPP.Models;
 using System.Collections.Generic;
 using System.Text;
+using System.Collections.ObjectModel;
 
 namespace MECANICAPP.ViewModels.EmpresaViewModel
 {
      public class EmpresaInicioViewModel
     {
+        public ObservableCollection<CategoryMap> CategoryMaps
+        {
+            get;
+            set;
+        }
+        public EmpresaInicioViewModel()
+        {
+            LoadingEmpresaInicioViewModel();
+        }
+
+            
+        void  LoadingEmpresaInicioViewModel()
+        {
+
+        }
+
     }
 }
